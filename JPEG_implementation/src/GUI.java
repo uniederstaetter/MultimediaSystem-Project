4e9e0edf63ef945a83a243636885e7a73b250122
Unterrainer <stephanunterrainer@yahoo.it>
2020-05-22 11:52:58 +0200

    Refactoring
    Optimized part of the code to make it mor efficient and readable.

--- JPEG_implementation/src/GUI.java
+++ JPEG_implementation/src/GUI.java
@@ -1,198 +1,203 @@
 
 import java.awt.BorderLayout;
-import java.awt.Container;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Toolkit;
 import java.io.File;
-import java.io.FileFilter;
 import java.io.IOException;
 import java.util.List;
 
 import javax.imageio.ImageIO;
-import javax.swing.*;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.highgui.HighGui;
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
 
+public class GUI extends JFrame {
 
-public class GUI {
-    
-    private static final String WINDOW_NAME = "JPEG Compression_implementation";
+	/** Just until I found the settings for this warning! */
+	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Members for window settings.
+	 */
+	private static String WINDOW_NAME = "JPEG Compression_implementation";
+	private int widthscreen = Toolkit.getDefaultToolkit().getScreenSize().width;
+	private int heightscreen = Toolkit.getDefaultToolkit().getScreenSize().height;
 
-    private Mat srcGray = new Mat();
-    private Mat dst = new Mat();
-    
-    
-    
-    private JFrame frame;
-    private JMenuBar menuBar;
-    private JMenu menu;
-    private JMenuItem openFileItem;
-    private JLabel imgLabel;
-    private Image img; //Image the user chooses to be compressed.
-    private String filepath;//file the user opens
-    private String imageFile;
-    Image selectedImg;
+	/**
+	 * Components of the window.
+	 */
+	private JMenuBar menuBar;
+	private JMenu menu;
+	private JMenuItem openFileItem;
+	private JLabel imgLabel;
 
-    
-    
-    public GUI() {
-    	
-        // Create and set up the window.
-        frame = new JFrame(WINDOW_NAME);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        //creates the menu bar.
-        menuBar=new JMenuBar();
-        
-        //build the menu.
-        menu=new JMenu("Menu");
-        menuBar.add(menu);
-        
-        openFileItem=new JMenuItem("Open");//adding the possibility to open a new image
-        menu.add(openFileItem);
-        
-        openFileItem.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent ev) {
-        	
-        		imageFile=chooseImage();
-        		
-        		if (selectedImg!=null) {
-        			deleteComponentsOfPane(frame.getContentPane(), img);//delete the old image
-                	addComponentsToPane(frame.getContentPane(), selectedImg);//add the new image
-        		}
-        		
-        		
-        
-        	}
-        });
-        
-        
-        //adds menu bar to the frame.
-        frame.setJMenuBar(menuBar);
-        
-        //displays default image
-        displayDefault();
-        
-        //adding a button for executing the compression
-        JButton doCompression=new JButton("Compress Image");
-        frame.getContentPane().add(doCompression, BorderLayout.SOUTH);
-        doCompression.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				//Mat dct_mat=JPEG_compr.dct(imageFile);//convert image to dct.
-				List<Mat> dct_converted=DCT.divideBlocksDCT(imageFile);
-				//Quantization.fillMat();
-				List<Mat> quantised=Quantization.quantise(dct_converted);
-				/*int width=DCT.height;
-				int height=DCT.width;
-				Mat zigzag=ZigZag.zigZag(quantised, width, height);
-				*/
-				
-				
+	private Mat srcGray = new Mat();
+	private Mat dst = new Mat();
+
+	private Image img; // Image the user chooses to be compressed.
+	private String filepath; // File the user opens
+	private final static String defaultImgPath = "lena_grey.png"; // The path of the default image.
+	Image selectedImg;
+
+	public GUI() {
+		// Create and set up the window.
+		super(WINDOW_NAME);
+		// If the user closes the window the program stops.
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Disables the functionality to resize the window.
+		this.setResizable(false);
+
+		// Creates the menu bar.
+		menuBar = new JMenuBar();
+
+		// Build the menu and add it to the menu bar.
+		menu = new JMenu("Menu");
+		// adding the possibility to open a new image
+		openFileItem = new JMenuItem("Open");
+		menu.add(openFileItem);
+		menuBar.add(menu);
+		// Adds menu bar to the frame.
+		this.setJMenuBar(menuBar);
+
+		// Adds the default image to the frame.
+		displayDefault();
+		
+		// Bis do!
+
+		// adding a button for executing the compression
+		JButton doCompression = new JButton("Compress Image");
+		this.getContentPane().add(doCompression, BorderLayout.SOUTH);
+		
+		// add here code for executing algorithm
+
+		// Set up the content pane.
+		// Image img = HighGui.toBufferedImage(srcGray);
+		// addComponentsToPane(frame.getContentPane(), img);
+
+		/** Action listeners. */
+
+		/**
+		 * Creates an action listener for the menu item "Open". When the user clicks on
+		 * the item a file chooser will open. After selecting an image the default image
+		 * or the image already chosen will be removed an the new selected image will be
+		 * added to the container.
+		 */
+		openFileItem.addActionListener(event -> { // Lambda expression to simplify code.
+
+			filepath = chooseImage();
+
+			if (selectedImg != null) {
+				removeImage();
+				addImage(selectedImg); // Add the new image
 			}
 		});
-        //add here code for executing algorithm
-       
-        
-      
-        // Set up the content pane.
-        //Image img = HighGui.toBufferedImage(srcGray);
-        //addComponentsToPane(frame.getContentPane(), img);
-        
-        // Use the content pane's default BorderLayout. No need for
-        // setLayout(new BorderLayout());
-        // Display the window.
-        frame.pack();//adjust it self automatically to the contents inside the frame.
-        frame.setVisible(true);
-        
-    }
-    
-    //used to set up the default selected picture for the compression
-    private void displayDefault() {
-    	 File defaultFile= new File("lena_grey.png");
-    	 imageFile="lena_grey.png";
-         //Setting default image
-         try {
-			img=ImageIO.read(defaultFile);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-         addComponentsToPane(frame.getContentPane(),img);
-    }
-  
-    
-  
-    //add an to the panel
-    private void addComponentsToPane(Container panel, Image img) {
-    	
-    	imgLabel = new JLabel(new ImageIcon(img));
-        panel.add(imgLabel, BorderLayout.CENTER);
-        frame.pack();
-    }
-    
-    //delete an image from the panel
-    private void deleteComponentsOfPane(Container panel, Image img) {
-    	 panel.remove(imgLabel);
-    	
-    }
-    
-    //user chooses a file from its own directory for the compression
-    private String chooseImage() {
-    	
-    	JFileChooser fileChooser= new JFileChooser();
-    	File file=null;
-    	
-    	//allows only images to be read.
-    	FileNameExtensionFilter imageFilter = new FileNameExtensionFilter(
-    		    "Image files", ImageIO.getReaderFileSuffixes());
+		
+		doCompression.addActionListener(event -> {
+				// Mat dct_mat=JPEG_compr.dct(imageFile);//convert image to dct.
+				List<Mat> dct_converted = DCT.divideBlocksDCT(defaultImgPath);
+				// Quantization.fillMat();
+				List<Mat> quantised = Quantization.quantise(dct_converted);
+				/*
+				 * int width=DCT.height; int height=DCT.width; Mat
+				 * zigzag=ZigZag.zigZag(quantised, width, height);
+				 */
+		});
 
-    	//Attaching Filter to JFileChooser object
-    	fileChooser.setFileFilter(imageFilter);
-    	
-    	int action=fileChooser.showSaveDialog(null);
-    	
-    	if (action == JFileChooser.APPROVE_OPTION) {
-    		
-    		file=fileChooser.getSelectedFile();
-    		filepath=fileChooser.getSelectedFile().getAbsolutePath();
-    		
-    	}else {
-    		JOptionPane.showMessageDialog(frame, "You did not select a file! Please try again!");
-    		return null;//user did not choose an image, so null is returned that the old image is not deleted from frame.
-    	}
-    	
-    	try {
-			 selectedImg=ImageIO.read(file);
+		// Display the window.
+		this.pack(); 			// Adjust it self automatically to the contents inside the frame. What?
+		// Sets the dimension and the position of the window. (Window should open in the
+		// middle of the screen.)
+		int width = this.getWidth();
+		int height = this.getHeight();
+		this.setBounds(widthscreen / 2 - width / 2, heightscreen / 2 - height / 2, width, height);
+		this.setVisible(true);	// Make the window visible.
+
+	}
+
+	// Used to set up the default selected picture for the compression
+	private void displayDefault() {
+		File defaultFile = new File(GUI.defaultImgPath);
+		// Setting default image
+		try {
+			img = ImageIO.read(defaultFile);
+		} catch (IOException e) {
+			// TODO Open error dialog here.
+			e.printStackTrace();
+		}
+		addImage(img);
+	}
+
+	/**
+	 * Adds a image to the content pane.
+	 * @param img The image to be added.
+	 */
+	private void addImage(Image img) {
+		imgLabel = new JLabel(new ImageIcon(img));
+		this.getContentPane().add(imgLabel, BorderLayout.CENTER);
+		this.pack();
+	}
+
+	/** Removes the image from the content pane. */
+	private void removeImage() {
+		this.getContentPane().remove(imgLabel);
+	}
+
+	/**
+	 * A file chooser will be created and displayed.
+	 * The file chooser allows the user to select a new image  file.
+	 * Only image files are supported.
+	 *
+	 * @return The path of the image or null if the user does not choose an image.
+	 */
+	private String chooseImage() {
+
+		JFileChooser fileChooser = new JFileChooser();
+		File file = null;
+
+		// Allows only images to be read.
+		FileNameExtensionFilter imageFilter = new FileNameExtensionFilter("Image files", ImageIO.getReaderFileSuffixes());
+
+		// Attaching Filter to JFileChooser object
+		fileChooser.setFileFilter(imageFilter);
+
+		int action = fileChooser.showSaveDialog(null);
+
+		
+		if (action == JFileChooser.APPROVE_OPTION) {
+			file = fileChooser.getSelectedFile();
+			filepath = fileChooser.getSelectedFile().getAbsolutePath();
+		} else {
+			JOptionPane.showMessageDialog(this, "You did not select a file! Please try again!");
+			return null;
+		}
+
+		/**
+		 * Thinking about to do it in a different way!
+		 */
+		try {
+			selectedImg = ImageIO.read(file);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	
+
 		return filepath;
-    	
-    }
-    
-    
-    public static void main(String[] args) {
-        // Load the native OpenCV library
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        // Schedule a job for the event dispatch thread:
-        // creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new GUI();
-            }
-        });
-    }
+	}
+
+	public static void main(String[] args) {
+		// Load the native OpenCV library
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		// Start the application!
+		new GUI();
+	}
 }
